6a7d5955e27bbe5da9266e8cf03a3463d0332bc2
oygulmez <oygulmez97@gmail.com>
2025-06-02 00:43:45 +0300

    TypeScript ve ESLint hatalarını düzelt: any tipini kaldır ve interface'leri düzelt

--- uydu-servisi/src/types/next-auth.d.ts
+++ uydu-servisi/src/types/next-auth.d.ts
@@ -1,4 +1,5 @@
 import 'next-auth'
+import { DefaultSession } from 'next-auth'
 
 declare module 'next-auth' {
   interface Session {
@@ -7,7 +8,7 @@ declare module 'next-auth' {
       email: string
       name: string
       role: string
-    }
+    } & DefaultSession['user']
   }
 
   interface User {
